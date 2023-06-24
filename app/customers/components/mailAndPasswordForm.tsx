680cdcb271e2f278d4bd2efb50107fc33a0fe8e4
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2023-06-24 17:04:22 +0900

    feat: 認証or新規登録が完了したらホームに遷移する処理を作成

--- app/customers/components/mailAndPasswordForm.tsx
+++ app/customers/components/mailAndPasswordForm.tsx
@@ -3,6 +3,11 @@
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { Button, Form, Input, Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
+import { auth } from '../../firebase/config';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import { NextRouter } from 'next/router';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
 
 interface Parameter {
   name: string;
@@ -33,14 +38,51 @@ const Loding = () => {
   );
 }
 
-function onCilick(setLoading: Dispatch<SetStateAction<boolean>>, parameter: Parameter) {
-  console.log(parameter);
+function onCilick(setLoading: Dispatch<SetStateAction<boolean>>, parameter: Parameter, router: AppRouterInstance) {
+  const { name, email, password, isLogin } = parameter;
   setLoading(true);
+
+  if (isLogin) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('login');
+        const user = userCredential.user;
+        console.log(userCredential);
+        router.push('/');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        window.alert('メールアドレスかパスワードが間違っています');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  } else {
+    console.log('login');
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('success');
+        const user = userCredential.user;
+        console.log(user);
+        router.push('/');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
 }
 
 export default function MailAndPasswordForm(props: {name?: string, buttonValue: string, isLogin: boolean}) {
   const { buttonValue, isLogin, name } = props;
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
   const [parameter, setParameter] = useState<Parameter>({
     name: name || '',
     email: '',
@@ -70,7 +112,7 @@ export default function MailAndPasswordForm(props: {name?: string, buttonValue: 
         <Button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold"
           htmlType="submit"
-          onClick={() => onCilick(setLoading, parameter)}
+          onClick={() => onCilick(setLoading, parameter, router)}
         >
           {buttonValue}
         </Button>
