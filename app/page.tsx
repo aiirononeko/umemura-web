307173895e897d14c9352ed044be8dd4a2fd9060
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2023-07-01 11:20:05 +0900

    feat: link

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ export default function Top() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold pb-10">梅村のサイト</h1>
-      <Link href={currentUser ? "/reservation" : "customers/login"} className="pb-10">
+      <Link href={currentUser ? "/reservation" : "signin"} className="pb-10">
         予約ページへ
       </Link>
-      <Link href={currentUser ? "/admin" : "customers/login"}>管理者ページへ</Link>
+      <Link href={currentUser ? "/admin" : "signin"}>管理者ページへ</Link>
     </main>
   );
 }
