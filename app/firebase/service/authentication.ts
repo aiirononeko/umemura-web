60fc7bb358feae6117b588e377d2decc732314fa
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2023-07-06 23:31:03 +0900

    一旦コミット

--- app/firebase/service/authentication.ts
+++ app/firebase/service/authentication.ts
@@ -4,6 +4,7 @@ import { auth } from "../config";
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  sendSignInLinkToEmail,
 } from "firebase/auth";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 import { Dispatch, SetStateAction } from "react";
@@ -86,24 +87,62 @@ export function registerStuffWithSendingEmail(
   backPath: string,
   haveToSetUser: boolean
 ) {
-  const { email } = data;
+  const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: 'http://localhost:3000?email=' + data.email,
+    // This must be true.
+    handleCodeInApp: true,
+    // iOS: {
+    //   bundleId: 'com.example.ios'
+    // },
+    // android: {
+    //   packageName: 'com.example.android',
+    //   installApp: true,
+    //   minimumVersion: '12'
+    // },
+    // dynamicLinkDomain: 'example.page.link'
+  };
+  console.log(actionCodeSettings)
   setLoading(true);
-  console.log("called");
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      const uid = user.uid;
-      addDocumentWithUid(data, "stuffs", uid);
-      if (haveToSetUser) {
-        setCurrentUser();
-      }
-      window.alert("登録が完了しました。");
-      router.push(backPath);
+  const { email } = data;
+  sendSignInLinkToEmail(auth, email, actionCodeSettings)
+    .then(() => {
+      // The link was successfully sent. Inform the user.
+      // Save the email locally so you don't need to ask the user for it again
+      // if they open the link on the same device.
+      console.log('success');
+      window.localStorage.setItem('emailForSignIn', email);
+      // ...
     })
-    .catch((_error) => {
-      window.alert(`既にユーザーが存在します`);
+    .catch((error) => {
+      console.log('error');
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      // ...
     })
     .finally(() => {
       setLoading(false);
     });
+  // const { email } = data;
+  // setLoading(true);
+  // console.log("called");
+  // createUserWithEmailAndPassword(auth, email, password)
+  //   .then((userCredential) => {
+  //     const user = userCredential.user;
+  //     const uid = user.uid;
+  //     addDocumentWithUid(data, "stuffs", uid);
+  //     if (haveToSetUser) {
+  //       setCurrentUser();
+  //     }
+  //     window.alert("登録が完了しました。");
+  //     router.push(backPath);
+  //   })
+  //   .catch((_error) => {
+  //     window.alert(`既にユーザーが存在します`);
+  //   })
+  //   .finally(() => {
+  //     setLoading(false);
+  //   });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,85 @@
 
 import Link from "next/link";
 import { auth } from "./firebase/config";
+import { Button, Center, Container, Modal, TextInput, Title } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
+
+const queryString = window.location.search;
+const parameter = new URLSearchParams(queryString);
+const email = parameter.get("email");
+
+const SetPassword = () => {
+  const form = useForm({
+    initialValues: {
+      email: email || "",
+      password: "",
+    },
+    validate: {
+      email: (value) =>
+        /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(value)
+          ? null : "メールアドレスを正しく入力してください",
+      password: (value) =>
+        value.length < 6 ? "パスワードは6文字以上で入力してください" : null,
+    },
+  });
+
+  return (
+    <Container className="m-auto " size="xs">
+      <Center>
+        <Title className="mb-12">パスワード登録</Title>
+      </Center>
+      <form
+        onSubmit={form.onSubmit((values) => {
+          console.log(values);
+        })}
+      >
+        <TextInput
+          size="xs"
+          label="メールアドレス"
+          placeholder="メールアドレス"
+          required
+          disabled
+          {...form.getInputProps("email")}
+          mb="lg"
+        />
+        <TextInput
+          size="xs"
+          label="パスワード"
+          placeholder="パスワード"
+          required
+          {...form.getInputProps("password")}
+          mb="lg"
+        />
+        <Center className="mt-4">
+          <Button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold"
+          >
+            登録
+          </Button>
+        </Center>
+      </form>
+    </Container>
+  );
+}
 
 export default function Top() {
   const currentUser = auth.currentUser;
+  const [opened, { open, close }] = useDisclosure(email ? true : false);
   console.log(currentUser);
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold pb-10">梅村のサイト</h1>
-      <Link href={currentUser ? "/reservation" : "signin"} className="pb-10">
-        予約ページへ
-      </Link>
-      <Link href={currentUser ? "/admin" : "signin"}>管理者ページへ</Link>
-    </main>
+    <>
+      <Modal opened={opened} onClose={close}>
+        <SetPassword />
+      </Modal>
+      <main className="flex min-h-screen flex-col items-center p-24">
+        <h1 className="text-4xl font-bold pb-10">梅村のサイト</h1>
+        <Link href={currentUser ? "/reservation" : "signin"} className="pb-10">
+          予約ページへ
+        </Link>
+        <Link href={currentUser ? "/admin" : "signin"}>管理者ページへ</Link>
+      </main>
+    </>
   );
 }
