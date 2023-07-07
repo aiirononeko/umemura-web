2fd389b2b234a02e7119c05f4c0c3a31b80bc0c9
aiirononeko <aiirononeko@gmail.com>
2023-07-07 14:03:41 +0900

    UI着手

--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -1,10 +1,53 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { Container, Stepper, Button, Group } from "@mantine/core";
 
 export default function Reservation() {
+  const [active, setActive] = useState(0);
+  const nextStep = () =>
+    setActive((current) => (current < 3 ? current + 1 : current));
+  const prevStep = () =>
+    setActive((current) => (current > 0 ? current - 1 : current));
+
+  const [staffs, setStaffs] = useState([]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold pb-10">予約</h1>
-      <Link href="/">トップページへ</Link>
-    </main>
+    <>
+      <Container className="m-auto">
+        <Stepper active={active} onStepClick={setActive} breakpoint="sm">
+          <Stepper.Step
+            label="First step"
+            description="スタッフを選択してください"
+          >
+            スタッフ選択画面
+          </Stepper.Step>
+          <Stepper.Step
+            label="Second step"
+            description="コースを選択してください"
+          >
+            コース選択画面
+          </Stepper.Step>
+          <Stepper.Step
+            label="Final step"
+            description="日にちを選択してください"
+          >
+            日にち選択画面
+          </Stepper.Step>
+          <Stepper.Completed>
+            予約が完了しました！ マイページから予約の確認ができます。
+          </Stepper.Completed>
+        </Stepper>
+
+        <Group position="center" mt="xl">
+          <Button variant="default" onClick={prevStep}>
+            戻る
+          </Button>
+          <Button onClick={nextStep}>進む</Button>
+        </Group>
+        <Link href="/">トップページへ</Link>
+      </Container>
+    </>
   );
 }
