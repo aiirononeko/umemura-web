9b4b313a7179b1cd9409430bbf417ac0224d5a30
ryotakatada <ryotakatada@ryotakatadanoMacBook-Pro.local>
2023-07-16 06:32:37 -0700

    fix

--- app/admin/reservation/page.tsx
+++ app/admin/reservation/page.tsx
@@ -2,41 +2,46 @@
 
 import Link from "next/link";
 import { useState, useEffect, useContext } from "react";
-import { Reservation, getDocuments, Customer, Stuff } from "@/app/firebase/service/collection";
+import {
+  Reservation,
+  getDocuments,
+  Customer,
+  Stuff,
+} from "@/app/firebase/service/collection";
 import { AuthContext } from "@/app/firebase/service/authContext";
 import { Timestamp } from "firebase/firestore";
 import { Button, Center, Container, Modal, Table, Title } from "@mantine/core";
 import { DatePicker } from "@mantine/dates";
 import { useDisclosure } from "@mantine/hooks";
 
-const ReservationDetail = (props: { reservation: Reservation, customers: Customer[] }) => {
-  const { reservation, customers } = props;
-  const customer = customers.find(c => c.id === reservation.customerId);
+const ReservationDetail = (props: { reservation: Reservation }) => {
+  const { reservation } = props;
 
   return (
     <>
       <Center>
-        <Title>
-          予約詳細
-        </Title>
+        <Title>予約詳細</Title>
       </Center>
       <h5>日時</h5>
       <h1>{`${reservation.startTime}~${reservation.endTime}`}</h1>
       <h5>コース名</h5>
       <h1>{reservation.course}</h1>
       <h5>お客様名</h5>
-      <h1>{`${customer?.lastName} ${customer?.firstName}`}</h1>
+      <h1>{`${reservation.customerName}`}</h1>
       <h5>電話番号</h5>
-      <h1>{customer?.phoneNumber}</h1>
+      <h1>{reservation.customerPhoneNumber}</h1>
+      <h5>メールアドレス</h5>
+      <h1>{reservation.customerEmail}</h1>
     </>
-  )
-}
+  );
+};
 
 export default function Top() {
   const uid = useContext(AuthContext).user?.uid;
-  const [originReservations, setOriginReservations] = useState<Reservation[]>([]);
+  const [originReservations, setOriginReservations] = useState<Reservation[]>(
+    []
+  );
   const [reservations, setReservations] = useState<Reservation[]>([]);
-  const [customers, setCustomers] = useState<Customer[]>([]);
   const [targetDate, setTargetDate] = useState(Timestamp.fromDate(new Date()));
   const [opened, { open, close }] = useDisclosure(false);
   const [num, setNum] = useState(0);
@@ -44,76 +49,84 @@ export default function Top() {
   useEffect(() => {
     (async () => {
       if (uid != undefined) {
-        const reservationDocuments = await getDocuments("reservations") as Reservation[];
+        const reservationDocuments = (await getDocuments(
+          "reservations"
+        )) as Reservation[];
         const filteredReservations = reservationDocuments.filter((r) => {
           return r.stuffId === uid;
         });
-        const targetCustomerIds = filteredReservations.map(doc => doc.customerId);
-        const customerDocuments = await getDocuments("customers") as Customer[]
-        const filteredCustomers = customerDocuments.filter(customer => targetCustomerIds.includes(customer.id));
-        setCustomers(filteredCustomers);
         setOriginReservations(filteredReservations);
-        setReservations(filteredReservations.filter(r => {
-          const date = r.date.toDate();
-          const tDate = targetDate.toDate();
-          const year = date.getFullYear();
-          const month = date.getMonth() + 1;
-          const day = date.getDate();
-          return year === tDate.getFullYear() && month === tDate.getMonth() + 1 && day === tDate.getDate();
-        }).sort((a, b) => {
-          if (a.startTime < b.startTime) {
-            return -1;
-          } else if (a.startTime > b.startTime) {
-            return 1;
-          } else {
-            return 0;
-        }}))
-      }})();
+        setReservations(
+          filteredReservations
+            .filter((r) => {
+              const date = r.date.toDate();
+              const tDate = targetDate.toDate();
+              const year = date.getFullYear();
+              const month = date.getMonth() + 1;
+              const day = date.getDate();
+              return (
+                year === tDate.getFullYear() &&
+                month === tDate.getMonth() + 1 &&
+                day === tDate.getDate()
+              );
+            })
+            .sort((a, b) => {
+              if (a.startTime < b.startTime) {
+                return -1;
+              } else if (a.startTime > b.startTime) {
+                return 1;
+              } else {
+                return 0;
+              }
+            })
+        );
+      }
+    })();
   }, [uid]);
 
   useEffect(() => {
-    const filteredDocuments = originReservations.filter(r => {
-      const date = r.date.toDate();
-      const tDate = targetDate.toDate();
-      const year = date.getFullYear();
-      const month = date.getMonth() + 1;
-      const day = date.getDate();
-      return year === tDate.getFullYear() && month === tDate.getMonth() + 1 && day === tDate.getDate();
-    }).sort((a, b) => {
-      if (a.startTime < b.startTime) {
-        return -1;
-      } else if (a.startTime > b.startTime) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
+    const filteredDocuments = originReservations
+      .filter((r) => {
+        const date = r.date.toDate();
+        const tDate = targetDate.toDate();
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        return (
+          year === tDate.getFullYear() &&
+          month === tDate.getMonth() + 1 &&
+          day === tDate.getDate()
+        );
+      })
+      .sort((a, b) => {
+        if (a.startTime < b.startTime) {
+          return -1;
+        } else if (a.startTime > b.startTime) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
     setReservations(filteredDocuments);
   }, [targetDate]);
 
   return (
     <Container>
       <Modal opened={opened} onClose={close} size="xs">
-        <ReservationDetail reservation={reservations.at(num)!} customers={customers} />
+        <ReservationDetail reservation={reservations.at(num)!} />
       </Modal>
       <Center mb="lg">
-        <Title>
-          予約管理日時
-        </Title>
+        <Title>予約管理日時</Title>
       </Center>
       <Center mb="lg">
-        <Button
-          component={Link}
-          href="/admin/availableTime"
-          className="mt-4"
-        >
+        <Button component={Link} href="/admin/availableTime" className="mt-4">
           予約可能日時を登録
         </Button>
       </Center>
       <Center>
         <DatePicker
           value={targetDate.toDate()}
-          onChange={e => setTargetDate(Timestamp.fromDate(e!))}
+          onChange={(e) => setTargetDate(Timestamp.fromDate(e!))}
           placeholder="Select date"
         />
       </Center>
@@ -149,10 +162,7 @@ export default function Top() {
         </tbody>
       </Table>
       <Center mt="xl">
-        <Button
-          component={Link}
-          href="/admin"
-        >
+        <Button component={Link} href="/admin">
           戻る
         </Button>
       </Center>
